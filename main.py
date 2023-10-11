6bb278618a147d29daefce6b8776dcdbf8df2579
lajkogege <lajkogege@gmail.com>
2023-10-11 12:10:39 +0200

    etlap.py

--- main.py
+++ main.py
@@ -1,5 +1,32 @@
 import etlap
 
+etlap_meret: int = 36
+
+etlap.kiiras("*", etlap_meret)
+
+etlap.szöveg("*", "Étlap", "*", etlap_meret)
+
+etlap.kiiras2("*", etlap_meret)
+
+etlap.szöveg2("*", "Főétel", "*", etlap_meret)
+
+etlap.kiiras3("*", etlap_meret)
+
+etlap.kis_szöveg("*", "Rakott krumpli:", "300Ft", "*", etlap_meret)
+
+etlap.kis_szöveg2("*", "Spenótos tészta:", "300Ft", "*", etlap_meret)
+
+etlap.kiiras4("*", etlap_meret)
+
+etlap.szöveg3("*", "Levesek", "*", etlap_meret)
+
+etlap.kiiras5("*", etlap_meret)
+
+etlap.kis_szöveg3("*", "Franciahagymaleves:", "300Ft", "*", etlap_meret)
+
+etlap.kis_szöveg4("*", ":", "300Ft", "*", etlap_meret)
+
+
 
 
 
